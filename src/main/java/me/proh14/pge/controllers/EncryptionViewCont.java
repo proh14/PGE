2b879514f2efa4a5a794bc4c6c5828139c1353e0
Hoorad <hourrad.f@gmail.com>
2022-09-09 20:09:21 +0430

    Added Event Handellers and ids to Encryption View

--- src/main/java/me/proh14/pge/controllers/EncryptionViewCont.java
+++ src/main/java/me/proh14/pge/controllers/EncryptionViewCont.java
@@ -1,5 +1,27 @@
 package me.proh14.pge.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class EncryptionViewCont {
 
+    @FXML
+    public Button encrypt;
+    @FXML
+    public Button paste;
+    @FXML
+    public Button filePicker;
+
+    public void onPaste(ActionEvent event) {
+
+
+
+    }
+
+    public void onEncrypt(ActionEvent event) {
+    }
+
+    public void onFilePicker(ActionEvent event) {
+    }
 }

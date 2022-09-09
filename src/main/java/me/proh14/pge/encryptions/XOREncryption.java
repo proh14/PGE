c4c45b8d0d9c9e11749cbcefdbf5e821b7d0e005
amiran <azimi6370@gmail.com>
2022-09-09 19:21:45 +0430

    update: XOREncryption is now singleton

--- src/main/java/me/proh14/pge/encryptions/XOREncryption.java
+++ src/main/java/me/proh14/pge/encryptions/XOREncryption.java
@@ -1,20 +1,36 @@
 package me.proh14.pge.encryptions;
 
-public class XOREncryption {
+public final class XOREncryption {
 
-    private static final long key = 1111111111111111L;
+    private static XOREncryption instance;
 
-    public static String Encrypt(String input) {
+    private long key = 1111111111111111L;
+
+    public static XOREncryption getInstance() {
+        if (instance == null)
+            instance = new XOREncryption();
+        return instance;
+    }
+
+    public String encrypt(String input) {
         StringBuilder encrypted = new StringBuilder();
         for (int i = 0; i < input.length(); i++)
             encrypted.append(input.charAt(i) ^ key);
         return encrypted.toString();
     }
 
-    public static String Decrypt(String input) {
+    public String decrypt(String input) {
         StringBuilder decrypted = new StringBuilder();
         for (int i = 0; i < input.length(); i++)
             decrypted.append(input.charAt(i) ^ key);
         return decrypted.toString();
+    }
+
+    public void setKey(long key) {
+        this.key = key;
+    }
+
+    public long getKey() {
+        return key;
     }
 }

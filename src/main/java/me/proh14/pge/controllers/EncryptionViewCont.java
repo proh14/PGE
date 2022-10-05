30844c384e6138aa40df143ab0eeff948203177c
proh14 <95998030+proh14@users.noreply.github.com>
2022-10-05 18:36:19 +0330

    Added file support to Encryption

--- src/main/java/me/proh14/pge/controllers/EncryptionViewCont.java
+++ src/main/java/me/proh14/pge/controllers/EncryptionViewCont.java
@@ -7,12 +7,16 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import me.proh14.pge.Main;
 import me.proh14.pge.encryptions.AESEncryption;
 
+import java.io.*;
+
 public class EncryptionViewCont {
 
+    private final FileChooser fileChooser = new FileChooser();
     @FXML
     public Button encrypt;
     @FXML
@@ -23,9 +27,9 @@ public class EncryptionViewCont {
     public TextArea inputText;
     @FXML
     public MenuItem undoEncryption;
-
-
+    private boolean openFile = true;
     private boolean isEncrypted = false;
+
 
     public void onCopyPaste(ActionEvent event) {
         Clipboard clipboard = Clipboard.getSystemClipboard();
@@ -49,6 +53,8 @@ public class EncryptionViewCont {
             filePicker.setText("Save to file");
             inputText.setText(AESEncryption.getInstance().encrypt(inputText.getText()));
             isEncrypted = true;
+            openFile = false;
+
         } else {
             isEncrypted = false;
             Stage stage = (Stage) encrypt.getScene().getWindow();
@@ -61,11 +67,44 @@ public class EncryptionViewCont {
             encrypt.setText("Encrypt");
             copyPaste.setText("Paste");
             filePicker.setText("Open from file");
+            openFile = true;
+
         }
 
     }
 
-    public void onFilePicker(ActionEvent event) {
+    public void onFilePicker(ActionEvent event) throws IOException {
+
+        if (openFile) {
+            File file = fileChooser.showOpenDialog(Main.getDecryptionScene().getWindow());
+            FileReader reader = new FileReader(file.getAbsoluteFile());
+            BufferedReader br = new BufferedReader(reader);
+            StringBuilder builder = new StringBuilder();
+            int curChar;
+            while ((curChar = br.read()) != -1) {
+                builder.append((char) curChar);
+            }
+
+            br.close();
+            reader.close();
+            inputText.setText(builder.toString());
+        } else {
+            File file = fileChooser.showSaveDialog(Main.getDecryptionScene().getWindow());
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter writer = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter wr = new BufferedWriter(writer);
+            int curChar;
+            String ourString = inputText.getText();
+
+            wr.write(ourString);
+
+
+            wr.close();
+            writer.close();
+        }
+
     }
 
 
@@ -81,6 +120,7 @@ public class EncryptionViewCont {
         copyPaste.setText("Paste");
         filePicker.setText("Open from file");
         undoEncryption.setDisable(true);
-        isEncrypted = false;
+        openFile = true;
+
     }
 }
